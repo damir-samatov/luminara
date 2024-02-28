e0f8b8f97a797218d22788f8d89d12d692aba9b2
damir-samatov <damirsamatofff@gmail.com>
2024-02-28 21:07:21 +0500

    fix: remove video from posts

--- components/PostItem.tsx
+++ components/PostItem.tsx
@@ -1,12 +1,11 @@
 "use server";
 import { FC } from "react";
-import { Post, Video, Image } from ".prisma/client";
+import { Post, Image } from ".prisma/client";
 import { onGetSignedFileReadUrl } from "@/actions/file.actions";
 import NextImage from "next/image";
 
 type PostItemProps = {
   post: Post & {
-    videos: Video[];
     images: Image[];
   };
 };
@@ -16,40 +15,17 @@ export const PostItem: FC<PostItemProps> = async ({ post }) => {
     post.images.map(async (image) => onGetSignedFileReadUrl({ key: image.key }))
   );
 
-  const videoUrls = await Promise.all(
-    post.videos.map(async (video) => onGetSignedFileReadUrl({ key: video.key }))
-  );
-
-  return (
-    <>
-      {imageUrls.map(
-        (res) =>
-          res.success && (
-            <NextImage
-              key={res.data.signedUrl}
-              className="w-96"
-              width="1600"
-              height="900"
-              src={res.data.signedUrl}
-              alt={post.title}
-            />
-          )
-      )}
-      {videoUrls.map(
-        (res) =>
-          res.success && (
-            <video
-              controls
-              autoPlay
-              muted
-              key={res.data.signedUrl}
-              className="w-96"
-              width="100"
-              height="100"
-              src={res.data.signedUrl}
-            />
-          )
-      )}
-    </>
+  return imageUrls.map(
+    (res) =>
+      res.success && (
+        <NextImage
+          key={res.data.signedUrl}
+          className="w-96"
+          width="1600"
+          height="900"
+          src={res.data.signedUrl}
+          alt={post.title}
+        />
+      )
   );
 };
